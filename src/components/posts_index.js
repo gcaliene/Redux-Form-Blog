a812b14dc7cced214fd71efa5e2c0b6a76c3a6d4
Windows <gersoncalienes@gmail.com>
2018-03-14 14:03:01 -0400

    Added a componentDidMount LC to PostsIndex component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    console.log('hello');
+    this.props.fetchPosts();
+  }
   render() {
     return <div>Posts Index</div>;
   }
 }
 
-export default PostsIndex;
+//you could pass the action creator this way instead of the mapDispatchToProps,
+export default connect(null, { fetchPosts })(PostsIndex);
