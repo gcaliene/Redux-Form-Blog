2304b412730c0373ff3240cc2e408e6cfedcc4f8
Windows <gersoncalienes@gmail.com>
2018-03-14 16:22:39 -0400

    List of posts are rendering

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -4,13 +4,37 @@ import { fetchPosts } from '../actions';
 
 class PostsIndex extends Component {
   componentDidMount() {
-    console.log('hello');
+    // console.log('hello');
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+    //can't use the same map you would use for arrays, u have to use the one from lodash
+    return _.map(this.props.posts, post => {
+      return (
+        <li key={post.id} className="list-group-item">
+          {post.title} hi
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <div>Posts Index</div>;
+    // console.log(this.props.posts);
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
 //you could pass the action creator this way instead of the mapDispatchToProps,
-export default connect(null, { fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,29 @@
 module.exports = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ['./src/index.js'],
   output: {
     path: __dirname,
     publicPath: '/',
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   devServer: {
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
     historyApiFallback: true,
     contentBase: './'
   }
