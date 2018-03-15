084047c1596ade490e7acd63d800b6c6415bfee7
Windows <gersoncalienes@gmail.com>
2018-03-15 15:34:44 -0400

    Added validate form reducer

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -31,7 +31,26 @@ class PostsNew extends Component {
   }
 }
 
+const validate = values => {
+  const errors = {};
+
+  if (!values.title || values.title.length < 3) {
+    errors.title = 'Enter a title that is atleast 3 characters long!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories!';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content!';
+  }
+
+  // if errors is empty, then the form is fine to submit
+  //if errors has any props, redux form assumes form is invalid
+  return errors;
+};
+
 //reduxForm is very similar to the connect function
 export default reduxForm({
+  validate: validate,
   form: 'PostsNewForm' //the name of the form
 })(PostsNew);
