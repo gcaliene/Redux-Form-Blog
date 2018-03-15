ccc43333b222c1bf1c1f1935e4aea1e6539539fa
Windows <gersoncalienes@gmail.com>
2018-03-15 13:58:53 -0400

    Updated New Post component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    //{...field.input is just a fancyand concise way to say onchange={field.input.onchange}, onHover={field.input.onHover}
+    return (
+      <div>
+        <input type="text" {...field.input} />
+      </div>
+    );
+  }
+
   render() {
-    return <form>New Post</form>;
+    return (
+      <form>
+        <Field name="title" component={this.renderTitleField} />
+      </form>
+    );
   }
 }
 
+//reduxForm is very similar to the connect function
 export default reduxForm({
   form: 'PostsNewForm' //the name of the form
 })(PostsNew);
